50cbc2d26481263eef969dfd4a3965c431721489
Tobias Stöger <tobias@more.io>
2018-12-13 10:26:22 +0100

    Initial commit
    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# microWakeupperESP
+# About the microWakeupper board
+
+The ESP supports different DeepSleep modes to reduce power consumption to a minimum.
+For more information check this doc:
+
+ ESP8266 -> https://www.espressif.com/sites/default/files/9b-esp8266-low_power_solutions_en_0.pdf
+
+If you google for "ESP deep sleep" there already some really cool examples/projects available,
+for example:
+
+ https://www.losant.com/blog/making-the-esp8266-low-powered-with-deep-sleep
+
+
+The microWakeupper board is NOT necessary, if the ESP just has to wakeup based on
+a sleep timer - don't forget to connect the pins GPIO16 and RST.
+
+BUT if you want to wakeup (additionally) through an external event (e.g. a switch, button or PIR-sensor)
+the microWakeupper makes your life much easier!
+
+
++ How to connect the microWakupper to your ESP?
+```
+ ESP                microWakeupper (Rev.2)
+ VCC (3.3)   <-->   VCC   (mandatory)
+ GND         <-->   GND   (mandatory)
+ RST         <-->   RST   (mandatory)
+ GPIO16      <-->   G16   (optional)
+     Not necessary if you don't use a sleep timer,
+     or GPIO16 is already connect to RST.
+ OUTPUT      <-->   DIS   (optional)
+     Just set DIS (disable pin) to HIGH, if you want
+     to disable the microWakeupper. This prevents further
+     resets as long as your code is running (until going to sleep again).
+ INPUT       <-->   STA   (optional)
+     This pin is HIGH, if the microWakeupper was triggered
+     within the last 2-3 seconds.
+```
+ (Check also the backside notes directly on the board ;-)
+
+
++ What else should you know? +++
+
+ - You don't have to take care about debouncing a connected button
+ - Recommended voltage is 3.3V
+ - You can connect buttons or switches with normally closed or normally opened logic
+ - There is a default timeout of 2-3 seconds for retriggering
+ - The jumper on the pcb has to be set to
+```
+     -> NC for mode "normally closed" or
+     -> NO for mode "normally opened"
+```
+
+ - If you like, just connect more than one microWakeupper to your ESP (see STA pin to check which one triggered)
+ - Current consumption:
+ ```
+     -> Mode NC     ~3uA
+     -> Mode NO     ~1nA
+     -> Active      ~1mA (feel free to remove the onboard LED ;-)
+```
+
++ The latest board revision is available on tindie: https://www.tindie.com/stores/moreiolabs/
+(Or make your own one based on the attached schematic.)
+
+The example code "microWakupperESP" is specially for a NodeMCU ESP-board, because the (second) internal LED (on pin D4) is used. Just remove/modify the relevant lines for other ESP breakouts, e.g. the Wemos D1 mini.
+
+That's it!
+
+Have fun and hopefully your project will run for years now ;-)
+
+Special thanks to @tinyledmatrix and @davedarko !
+
+Created 13 Dec 2018 by Tobias Stöger (@tstoegi, @moreioLabs)  
--- microWakeupperESP.ino
+++ microWakeupperESP.ino
@@ -1,129 +1,68 @@
 /*
- * microWakeupperESP
- * 
- * Example code for "How to integrate the microWakeupper board into your own projects?"
- * This example is specially for the ESP8266 (basically the ESP32 should work too).
- * 
- * The ESP supports different DeepSleep modes to reduce power consumption to a minimum.
- * For more information check this doc: 
- * 
- *  https://www.espressif.com/sites/default/files/9b-esp8266-low_power_solutions_en_0.pdf
- *  
- * If you google for "ESP deep sleep" there alredy some really cool examples/projects available, 
- * for example: 
- *  https://www.losant.com/blog/making-the-esp8266-low-powered-with-deep-sleep
- *  
- * The microWakeupper board is NOT necessary, if the ESP just has to wakeup based on 
- * a sleep timer - don't forget to connect the pins GPIO16 and RST.
- * BUT if you want to wakeup (additionally) through an external event (e.g. a switch, button or PIR-sensor)
- * the microWakeupper makes your life much easier!
- * 
- * 
- * How to connect the microWakupper to your ESP?
- *  (Check also the backside notes directly on the board ;-)
- *  
- *  :: ESP :::        ::: microWakeupper :::
- *  VCC (3.3)   <-->  VCC   (mandatory)
- *  GND         <-->  GND   (mandatory)
- *  RST         <-->  RST   (mandatroy)
- *  GPIO16      <-->  G16   (optional)
- *      Not necessary if you don't use a sleep timer, or GPIO16 is already connect to RST.
- *  OUTPUT      <-->  DIS   (optional)
- *      Just set DIS (disable pin) to HIGH, if you want to disable the microWakeupper. 
- *      This prevents further resets as long as your code is running (until going to sleep again).
- *  INPUT       <-->  STA   (optional)
- *      This pin is HIGH, if the microWakeupper was triggered within the last 2-3 seconds.
- * 
- *  
- * 
- * 
- *
- * 
- * 
- * This example code is in the public domain.
- * 
- * created 13 Dec 2018
- * by Tobias Stöger (@tstoegi, @moreioLabs)
- * 
- * 
- * Special thanks to @tinyledmatrix and @davedarko!
- * 
- */
+   microWakeupperESP
 
-#include <ESP8266WiFi.h>
-#include "Ticker.h"
+   Example code for "How to integrate the microWakeupper board into your own projects?"
+   This example is specially for connecting a NodeMCU(ESP8266) (basically the ESP32 should work too).
 
-/*
-  Log function to help debugging running code.
+   Please check the README for further information.
+
+   created 13 Dec 2018
+   by Tobias Stöger (@tstoegi, @moreioLabs)
+
+   Special thanks to @tinyledmatrix and @davedarko!
 */
-#ifndef DEBUGUTILS_H
-#define DEBUGUTILS_H
-#define Log(str)        \
-  Serial.print(__LINE__);     \
-  Serial.print(' ');          \
-  Serial.print(millis());     \
-  Serial.print(": ");         \
-  Serial.print(__PRETTY_FUNCTION__); \
-  Serial.print(' ');          \
-  Serial.println(str);
-#endif
 
-Ticker blinker;
- 
-#define LED D5  //On board LED
+// Remove comment, if you use wifi in your project
+// #include <ESP8266WiFi.h>
 
-void ICACHE_RAM_ATTR onTimerISR() {
-  digitalWrite(LED, !(digitalRead(LED))); //Toggle LED Pin
-  timer1_write(600000);//12us
-}
+
+#define NODEMCU_ESP12_LED D4  // Do NOT use LED_BUILTIN because usually it's connected to GPIO16 (D0)
+#define STA               D6  // D6 connected to STA pin
+#define DIS               D7  // D7 connected to DIS pin
+#define secondsToSleep    42  // Example time in seconds to sleep
+
 void setup() {
-  // put your setup code here, to run once:
-  // Serial
   Serial.begin(115200);
-  Log("Serial begin")
-  pinMode(D6, INPUT); //STA
-  pinMode(LED, OUTPUT); //LED
+  Serial.println("Serial begin");
 
-  //Initialize Ticker every 0.5s
-  timer1_attachInterrupt(onTimerISR);
-  timer1_enable(TIM_DIV16, TIM_EDGE, TIM_SINGLE);
-  timer1_write(600000); //120000 us
+  pinMode(NODEMCU_ESP12_LED, OUTPUT);       // Initialize the LED_BUILTIN pin as an output
+  
+  pinMode(STA, INPUT);                // Initialize the pin for STA as an input
 
+  pinMode(DIS, OUTPUT);               // Initialize the pin for DIS as an output
+  digitalWrite(DIS, HIGH);            // Disable the microWakeupper to prevent retriggering
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  testDeepSleep();
+  Serial.println("Hi there!");
+
+  // This is how you check what happened (during your sleep)
+  if (digitalRead(STA) == HIGH) {     // If HIGH the microWakeupper was triggered (by the connected switch)
+    Serial.println("ESP was reseted/wakeup by microWakeupper");
+  } else {                            // If LOW it points to the sleep timer (or reset button, or restart). If you need further details check system_get_rst_info()
+    Serial.println("Looks like ESP was reseted/wakeup by internal timer");
+  }
+
+  // Place you code here (whatever you will do, e.g. send something to IFTTT) >...
+  digitalWrite(NODEMCU_ESP12_LED, LOW); // Turn on internal LED
+  delay(32);                            // Just wait (during this time you can test your switch - nothing should happen if the microWakeupper is disabled)
+  digitalWrite(NODEMCU_ESP12_LED, LOW); // Turn off internal LED
+  // ...<
+
+  deepSleep(secondsToSleep);            // Sleep for n-seconds (during sleep you can test your switch - a wakeup should happen)
 }
 
 void deepSleep(long seconds)
 {
-  // Sleep
-  Log("ESP8266 in sleep mode");
-  WiFi.disconnect();
-  WiFi.mode(WIFI_OFF);
-  delay(1);
-  WiFi.forceSleepBegin();
-  Log("Going into deepSleep");
-  ESP.deepSleep(seconds * 1000000); //Microseconds
-  delay(200); //Muss angeblich nach deepSleep gemacht werden
+  // Remove the comments, if you use wifi in your project
+  //  WiFi.disconnect();
+  //  WiFi.mode(WIFI_OFF);
+  //  delay(1);
+  //  WiFi.forceSleepBegin();
+
+  Serial.println("Going into deepSleep now");
+  ESP.deepSleep(seconds * 1000000);   // ESP.deepSleep expects microseconds
+  delay(200);                         // Seems recommended after calling deepSleep
 }
 
-void testDeepSleep() {
-
-  Log("testDeepSleep");
-
-  if (digitalRead(D6) == HIGH) {
-    Log("ESP was reseted by microWakeupper")
-  } else {
-    Log("ESP was reseted by internal timer [ESP.deepSleep(MicroSenconds)]")
-  }
-  Log("Will go back into DeepSleep in 6 seconds...")
-
-  pinMode(D7, OUTPUT); //DIS
-  digitalWrite(D7, HIGH);
-  delay(6000);
-
-  Log("...gn8 (I will sleep for 10 seconds)");
-  deepSleep(10);
-}
